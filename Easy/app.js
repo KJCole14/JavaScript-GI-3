00d2685f9e00e9b8e3f997d32e3493dcadb70298
KJCole14 <kabahjcole@gmail.com>
2022-09-28 18:43:52 -0400

    Easy GI complete

---
+++ Easy/app.js
@@ -0,0 +1,15 @@
+//EASY: Write a function that would allow you to do this:
+
+// var run = exercise('running');
+// console.log(run()); // prints "Today's exercise: running"
+// var swim = exercise('swimming');
+// console.log(swim()); // prints "Today's exercise: running"
+
+function activity(exercise) {
+    if (exercise === "running") {
+        console.log("Today's exercise: " + exercise);
+    } else if (exercise === "swimming") {
+        console.log("Today's exercise: " + exercise);
+    }
+}
+console.log(activity("swimming"));
